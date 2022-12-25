e34f7d24b6163056f888e0a015d27297d8b246bc
jaehak24 <65396939+jaehak24@users.noreply.github.com>
2022-12-25 22:36:37 +0900

    TDD 실습

--- untitled/src/test/java/org/example/PasswordValidatorTest.java
+++ untitled/src/test/java/org/example/PasswordValidatorTest.java
@@ -10,8 +10,22 @@ class PasswordValidatorTest {
     @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다.")
     @Test
     void validPasswordTest() {
-        assertThatCode(() -> PasswordValidator.validate("serverWizard")).
+        assertThatCode(() -> PasswordValidator.validate("serverWizard")). //12자
                 doesNotThrowAnyException();
+    }
+
+    @DisplayName("비밀번호가 최소 8자 이상, 12자 이하면 예외가 발생하지 않는다. 2")
+    @Test
+    void validPasswordTest2(){
+        assertThatCode(()-> PasswordValidator.validate("Terarosa")).doesNotThrowAnyException();
+    }
+
+
+    @DisplayName("비밀번호가 8자 미만 또는 12자 초과하는 경우 IlleagalExpressionException 예외가 발생한다.")
+    @Test
+    void validatePasswordTest2(){
+        assertThatCode(()->PasswordValidator.validate("aabb"))
+                .isInstanceOf(IllegalArgumentException.class).hasMessage("비밀번호는 최소 8자 이상 12자 이하여야 한다.");
     }
 
 }
